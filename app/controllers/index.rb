00a2b8a363d129f21ffeff063677dc808d1a637b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-30 16:51:35 -0500

    database caching complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,13 +4,24 @@ get '/' do
 end
 
 get '/:handle' do
-  @user = User.find_or_create_by_username(params[:handle])
-  tweets = Twitter.user_timeline(params[:handle],:count => 10, :include_rts => false)
-  @tweets = []
-  tweets.each { |tweet| @tweets << Tweet.create(user_id: @user.id, text: tweet.text, created_at: Time.now) }
+  # if User.find_by(username: params[:handle])
+  if @user = User.find_by_username(params[:handle])
+    if Time.now - @user.tweets.first.created_at > 15000
+      tweets = Twitter.user_timeline(params[:handle],:count => 10, :include_rts => false)
+      @tweets = []
+      tweets.each { |tweet| @tweets << Tweet.create(user_id: @user.id, text: tweet.text, created_at: Time.now) }
+    else
+      @tweets = @user.tweets.limit(10)
+    end
+  else
+    @user = User.create(username: params[:username])
+    tweets = Twitter.user_timeline(params[:handle],:count => 10, :include_rts => false)
+    @tweets = []
+    tweets.each { |tweet| @tweets << Tweet.create(user_id: @user.id, text: tweet.text, created_at: Time.now) }
+  end
   erb :tweets
 end
 
-post '/tweets' do   
+post '/tweets' do 
   redirect to("/#{params[:handle]}")
 end
