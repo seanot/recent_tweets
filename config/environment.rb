1b252a54382ae4bd69d79b86b1cd472d25f47fd2
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-30 14:43:09 -0500

    added yaml file for keys

--- config/environment.rb
+++ config/environment.rb
@@ -31,3 +31,8 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['TWITTER_KEY']
+  config.consumer_secret = ENV['TWITTER_SECRET']
+end
